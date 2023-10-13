cdacec990c6c8ea6073477a5d359912cf06b947e
LaPaNu4 <120036931+LaPaNu4@users.noreply.github.com>
2023-10-13 14:11:04 +0300

    setings

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
+import { lazy } from "react";
+import { Suspense } from "react"
+import { HashRouter, Route, Routes } from "react-router-dom";
+
+
+const Header = lazy(() => import('./header/header'));
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Suspense fallback={<h2>Loading...</h2>}>
+        <HashRouter>
+          <Header />
+          <main>
+            <Routes>
+              <Route path="/" element={<h1>My account</h1>} />
+              <Route path="/calendar" element={<h1>Calendar</h1>} />
+              <Route path="/statistics" element={<h1>Statistics</h1>} />
+            </Routes>
+          </main>
+        </HashRouter>
+      </Suspense>
+    </>
   );
 };
