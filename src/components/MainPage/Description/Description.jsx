4a596589d727225be4f67aa86396c73280133512
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-10-16 23:20:10 +0300

    add desciption

---
+++ src/components/MainPage/Description/Description.jsx
@@ -0,0 +1,125 @@
+import {
+  Container,
+  HiddenH2,
+  Ul,
+  Li,
+  TitleH3,
+  NumberSpan,
+  AccentH3,
+  Text,
+  ThumbDiv,
+} from './Description.styled';
+
+import calendar_mob from '../../../images/Description/calendar-mob.jpg';
+import calendar_mob2x from '../../../images/Description/calendar-mob2x.jpg';
+import calendar_tab from '../../../images/Description/calendar-tab.jpg';
+import calendar_tab2x from '../../../images/Description/calendar-tab2x.jpg';
+import calendar_desk from '../../../images/Description/calendar-desk.jpg';
+import calendar_desk2x from '../../../images/Description/calendar-desk2x.jpg';
+
+import sidebar_mob from '../../../images/Description/sidebar-mob.jpg';
+import sidebar_mob2x from '../../../images/Description/sidebar-mob2x.jpg';
+import sidebar_tab from '../../../images/Description/sidebar-tab.jpg';
+import sidebar_tab2x from '../../../images/Description/sidebar-tab2x.jpg';
+import sidebar_desk from '../../../images/Description/sidebar-desk.jpg';
+import sidebar_desk2x from '../../../images/Description/sidebar-desk2x.jpg';
+
+import all_in_one_mob from '../../../images/Description/all_in_one_mob.jpg';
+import all_in_one_mob2x from '../../../images/Description/all_in_one_mob2x.jpg';
+import all_in_one_tab from '../../../images/Description/all_in_one_tab.jpg';
+import all_in_one_tab2x from '../../../images/Description/all_in_one_tab2x.jpg';
+import all_in_one_desk from '../../../images/Description/all_in_one_desk.jpg';
+import all_in_one_desk2x from '../../../images/Description/all_in_one_desk2x.jpg';
+
+export const Description = () => {
+  return (
+    <Container>
+      <HiddenH2>Description</HiddenH2>
+      <Ul>
+        <Li>
+          <div>
+            <TitleH3>
+              <NumberSpan>1.</NumberSpan>
+              <AccentH3>Calendar</AccentH3>
+              View
+            </TitleH3>
+            <Text>"GooseTrack's Calendar view"</Text>
+          </div>
+          <ThumbDiv>
+            <picture>
+              <source
+                media="(max-width: 767px)"
+                srcSet={`${calendar_mob} 1x, ${calendar_mob2x} 2x`}
+              />
+              <source
+                media="(max-width: 1279px)"
+                srcSet={`${calendar_tab} 1x, ${calendar_tab2x} 2x`}
+              />
+              <source
+                media="(min-width: 1280px)"
+                srcSet={`${calendar_desk} 1x, ${calendar_desk2x} 2x`}
+              />
+
+              <img src={`${calendar_mob}`} alt="Calendar landing" />
+            </picture>
+          </ThumbDiv>
+        </Li>
+        <Li>
+          <div>
+            <TitleH3>
+              <NumberSpan>2.</NumberSpan>
+              'Sidebar'
+            </TitleH3>
+            <Text>'GooseTrack offers easy access'</Text>
+          </div>
+          <ThumbDiv>
+            <picture>
+              <source
+                media="(max-width: 767px)"
+                srcSet={`${sidebar_mob} 1x, ${sidebar_mob2x} 2x`}
+              />
+              <source
+                media="(max-width: 1279px)"
+                srcSet={`${sidebar_tab} 1x, ${sidebar_tab2x} 2x`}
+              />
+              <source
+                media="(min-width: 1280px)"
+                srcSet={`${sidebar_desk} 1x, ${sidebar_desk2x} 2x`}
+              />
+
+              <img src={`${sidebar_mob}`} alt="Calendar landing" />
+            </picture>
+          </ThumbDiv>
+        </Li>
+        <Li>
+          <div>
+            <TitleH3>
+              <NumberSpan>3.</NumberSpan>
+              <AccentH3>'All in'</AccentH3>
+              'One'
+            </TitleH3>
+            <Text>'GooseTrack is an all-in-one'</Text>
+          </div>
+          <ThumbDiv>
+            <picture>
+              <source
+                media="(max-width: 767px)"
+                srcSet={`${all_in_one_mob} 1x, ${all_in_one_mob2x} 2x`}
+              />
+              <source
+                media="(max-width: 1279px)"
+                srcSet={`${all_in_one_tab} 1x, ${all_in_one_tab2x} 2x`}
+              />
+              <source
+                media="(min-width: 1280px)"
+                srcSet={`${all_in_one_desk} 1x, ${all_in_one_desk2x} 2x`}
+              />
+
+              <img src={`${all_in_one_mob}`} alt="Calendar landing" />
+            </picture>
+          </ThumbDiv>
+        </Li>
+      </Ul>
+    </Container>
+  );
+};
---
+++ src/components/MainPage/Description/Description.styled.js
@@ -0,0 +1,130 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+  padding: 64px 20px;
+  margin: 0 auto;
+  max-width: 375px;
+
+  @media screen and (min-width: 768px) {
+    max-width: 768px;
+    padding: 64px 32px;
+  }
+
+  @media screen and (min-width: 1024px) {
+    max-width: 1440px;
+    padding: 118px 128px;
+  }
+`;
+
+export const HiddenH2 = styled.h2`
+  visibility: hidden;
+  width: 1px;
+  height: 1px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  transform: translate(-1px, -1px);
+`;
+
+export const Ul = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+  display: flex;
+  flex-direction: column;
+  gap: 64px;
+`;
+
+export const Li = styled.li`
+  display: inline-flex;
+  flex-direction: column;
+  :nth-child(2n) {
+    align-items: flex-end;
+  }
+
+  @media screen and (min-width: 1024px) {
+    max-width: 1107px;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    gap: 16vw;
+
+    :nth-child(2n) {
+      flex-direction: row-reverse;
+      align-items: center;
+    }
+    :nth-child(2n-1) {
+      margin-left: auto;
+    }
+  }
+`;
+
+export const TitleH3 = styled.h3`
+  display: inline-flex;
+  flex-direction: column;
+  color: var(--heading-text-color);
+  font-size: 32px;
+  line-height: 1.25;
+  font-family: var(--main-font);
+  font-weight: 700;
+  text-transform: uppercase;
+  margin-bottom: 14px;
+
+  @media screen and (min-width: 768px) {
+    max-width: 275px;
+    font-size: 40px;
+    line-height: 1.1;
+    margin-bottom: 24px;
+  }
+`;
+
+export const NumberSpan = styled.span`
+  color: var(--blue);
+  font-size: 80px;
+  line-height: 1;
+  letter-spacing: -4px;
+  margin-bottom: 14px;
+
+  @media screen and (min-width: 768px) {
+    font-size: 104px;
+    line-height: 1;
+    letter-spacing: -4px;
+  }
+`;
+
+export const AccentH3 = styled.span`
+  color: var(--blue);
+  font-size: 32px;
+  line-height: 1.25;
+  text-transform: uppercase;
+  padding: 8px 19px;
+  border-radius: 44px;
+  background: var(--modal-backdrop);
+  margin-bottom: 8px;
+  overflow: hidden;
+
+  @media screen and (min-width: 768px) {
+    font-size: 40px;
+    line-height: 1.1;
+  }
+`;
+
+export const Text = styled.p`
+  color: var(--description-text-color);
+  font-size: 14px;
+  line-height: 1.29;
+  font-family: var(--main-font);
+  font-weight: 500;
+
+  @media screen and (min-width: 768px) {
+    max-width: 275px;
+  }
+`;
+
+export const ThumbDiv = styled.div`
+  margin-top: 40px;
+
+  @media screen and (min-width: 1024px) {
+    margin-top: 0;
+  }
+`;
