42c2b28bed92f037bef740b7b5ff28f83449cbc5
LarisaBeskrovna <117515454+LarisaBeskrovna@users.noreply.github.com>
2023-10-20 12:42:10 +0300

    dark-light

---
+++ src/components/ThemeToggler/themeToggler.styled.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  border: none;
+  width: 24px;
+  height: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #f7f6f9;
+  @media screen and (min-width: 768px) {
+    width: 32px;
+    height: 32px;
+  }
+`;
+
+export const Vector = styled.svg`
+  width: 20px;
+  height: 20px;
+
+  @media screen and (min-width: 768px) {
+    width: 26px;
+    height: 26px;
+  }
+`;
+export const Div = styled.div``;
---
+++ src/hooks/useTheme.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+
+const isDarkTheme = window?.matchMedia('(prefers-color-scheme: dark)').matches;
+const defaultTheme = isDarkTheme ? 'dark' : 'light';
+
+export const useTheme = () => {
+  const [theme, setTheme] = useState(
+    localStorage.getItem('app-theme') || defaultTheme,
+  );
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('app-theme', theme);
+  }, [theme]);
+
+  return { theme, setTheme };
+};
