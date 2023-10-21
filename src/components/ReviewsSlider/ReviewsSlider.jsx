4dfb12fd83c55c0988427f3ad9b265b2e123e001
Valeriya Pavlova <vla.valeriya@gmail.com>
2023-10-21 18:16:29 +0300

    update

--- src/components/ReviewsSlider/ReviewsSlider.jsx
+++ src/components/ReviewsSlider/ReviewsSlider.jsx
@@ -2,6 +2,8 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import avatar from '../../images/ReviewSlider/avatarReview.jpg';
+import leftArrow from '../../images/ReviewSlider/left-arrow.svg';
+import rightArrow from '../../images/ReviewSlider/right-arrow.svg';
 
 import reviews from '../../components/ReviewsSlider/reviews.json';
 import {
@@ -15,10 +17,39 @@ import {
   WrapperReviewCommon,
 } from './ReviewsSlider.styled';
 
+function SamplePrevArrow(props) {
+  const { onClick } = props;
+  return (
+    <button onClick={onClick}>
+      {/* <Svg width="61" height="50">
+        <use href={`${rightArrow}#icon-arrow-right`} />
+      </Svg> */}
+    </button>
+  );
+}
+
+function SampleNextArrow(props) {
+  const { onClick } = props;
+  return (
+    <button onClick={onClick}>
+      {/* <Svg width="61" height="50">
+        <use href={`${sprite}#icon-arrow-left`} />
+      </Svg> */}
+    </button>
+  );
+}
+
 export const ReviewsSlider = () => {
   const settings = {
     dots: false,
     infinite: true,
+<<<<<<< Updated upstream
+=======
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+>>>>>>> Stashed changes
     autoplay: true,
     autoplaySpeed: 5000,
     cssEase: 'linear',
