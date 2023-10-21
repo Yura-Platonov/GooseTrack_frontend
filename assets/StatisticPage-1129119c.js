cff631d0827393c45b0b6ef423fc9db6ee777a8b
LaPaNu4 <120036931+LaPaNu4@users.noreply.github.com>
2023-10-21 17:23:10 +0000

    Deploying to gh-pages from @ Yura-Platonov/GooseTrack_frontend@aea7d8aacd993b9c0012518abd8172f4e3f518a2 🚀

--- assets/StatisticPage-1129119c.js
+++ assets/StatisticPage-1129119c.js
@@ -1,4 +1,4 @@
-import{s as It,j as Q,a as ce,c as ni,r as L,R as _,P as X,e as jv}from"./index-de883bfc.js";import{C as Cv,T as Iv,P as kv}from"./CalendarToolbar.styled-9d1e9f18.js";import{a as ae}from"./index-b9820f57.js";const Dv=It.ul`
+import{s as It,j as Q,a as ce,c as ni,r as L,R as _,P as X,e as jv}from"./index-110f0cff.js";import{C as Cv,T as Iv,P as kv}from"./CalendarToolbar.styled-611674ad.js";import{a as ae}from"./index-b201a440.js";const Dv=It.ul`
   display: flex;
 
   list-style: none;
