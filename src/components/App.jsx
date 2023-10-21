ab779fed23b18f58feecaa93ef65225ddc916320
Yura <113457886+Yura-Platonov@users.noreply.github.com>
2023-10-21 23:33:12 +0300

    add routes to user

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,52 +1,3 @@
-// import { lazy, useEffect, useState, Suspense } from 'react';
-// import { Route, Routes } from 'react-router-dom';
-// import { Section } from './App.styled';
-
-// const MainPage = lazy(() => import('./MainPage/MainPage'));
-// const Header = lazy(() => import('./header/header'));
-// const StatisticPage = lazy(() => import('../pages/StatisticPage'));
-// const Sidebar = lazy(() => import('./sidebar/sidebar'));
-// const CalendarPage = lazy(() => import('../pages/CalendarPage'));
-// const Login = lazy(() => import('./Login/Login'));
-// const SignUp = lazy(() => import('./SignUp/SignUp'));
-
-// export const App = () => {
-//   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-//   useEffect(() => {
-//     const handleResize = () => {
-//       setWindowWidth(window.innerWidth);
-//     };
-//     window.addEventListener('resize', handleResize);
-//     return () => {
-//       window.removeEventListener('resize', handleResize);
-//     };
-//   }, []);
-
-//   return (
-//     <>
-//       <Suspense fallback={<h2>Loading...</h2>}>
-//         <Section>
-//           <div>{windowWidth >= 1440 ? <Sidebar /> : null}</div>
-//           <div>
-//             <header>
-//               <Header />
-//             </header>
-//             <main>
-//               <Routes>
-//                 <Route path="/" element={<MainPage />} />
-//                 <Route path="/calendar" element={<CalendarPage />} />
-//                 <Route path="/statistics" element={<StatisticPage />} />
-//                 <Route path="/login" element={<Login />} />
-//                 <Route path="/register" element={<SignUp />} />
-//               </Routes>
-//             </main>
-//           </div>
-//         </Section>
-//       </Suspense>
-//     </>
-//   );
-// };
 import { lazy, Suspense, useEffect } from 'react';
 import { refresh } from '../redux/auth/operations';
 import { useDispatch, useSelector } from 'react-redux';
@@ -68,6 +19,7 @@ const StatisticsPage = lazy(() => import('../pages/StatisticPage'));
 const LoginPage = lazy(() => import('../pages/LoginPage/LoginPage'));
 const RegisterPage = lazy(() => import('../pages/RegisterPage/RegisterPage'));
 const PageNotFound = lazy(() => import('../pages/PageNotFound/PageNotFound'));
+const UserProfilePage = lazy(() => import('../pages/UserProfilePage/UserProfilePage'));
 
 export function App() {
   const dispatch = useDispatch();
@@ -87,12 +39,14 @@ export function App() {
           </Route>
           <Route path="/" element={<PrivateRoute />}>
             <Route path="/" element={<MainLayout />}>
+            <Route path="account" element={<UserProfilePage/>} />
               <Route path="calendar" element={<CalendarPage />}>
                 <Route path="month/:currentDate" element={ChoosedMonth} />
                 <Route path="day/:currentDate" element={ChoosedDay} />
               </Route>
               <Route path="statistics" element={<StatisticsPage />} />
             </Route>
+            
           </Route>
 
           <Route path="*" element={<PageNotFound />} />
