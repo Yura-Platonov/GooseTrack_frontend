a169c05c367fad8f4f4e5c2a6760a9201fc99ce5
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-10-24 00:39:46 +0300

    Update TasksColumnsList.jsx

--- src/components/calendar/tasksColumnsList/TasksColumnsList.jsx
+++ src/components/calendar/tasksColumnsList/TasksColumnsList.jsx
@@ -3,27 +3,13 @@ import React from 'react';
 import { TaskColumnCard } from '../TaskColumnCard/TaskColumnCard';
 import { TasksList } from './TasksColumnsList.styled';
 
-export const ColumnsTasksList = ({ tasks, title, selectedDate }) => {
+export const ColumnsTasksList = ({ tasks = [], getTask }) => {
   return (
     <>
-      <TasksList title={title}>
-        {tasks &&
-          tasks.map((task) =>
-            task.data.map((task) => (
-              <TaskColumnCard
-                selectedDate={selectedDate}
-                key={task.createdAt}
-                description={task.title}
-                avatarUrl={task.owner.avatarURL}
-                id={task._id}
-                date={task.date}
-                category={task.category}
-                priority={task.priority}
-                start={task.start}
-                end={task.end}
-              />
-            )),
-          )}
+      <TasksList>
+        {tasks?.map((task) => (
+          <TaskColumnCard task={task} key={task._id} getTask={getTask} />
+        ))}
       </TasksList>
     </>
   );
