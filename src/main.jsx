2f32b1c4d669f30878ab9324fc37e82c0992fa35
LaPaNu4 <120036931+LaPaNu4@users.noreply.github.com>
2023-10-26 20:00:19 +0300

    ad

--- src/main.jsx
+++ src/main.jsx
@@ -6,18 +6,18 @@ import { BrowserRouter } from 'react-router-dom';
 import { persistor, store } from './redux/store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { ProviderTheme } from 'components/ProviderTheme/ProviderTheme'
+// import { ProviderTheme } from 'components/ProviderTheme/ProviderTheme'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-      <ProviderTheme>
+      {/* <ProviderTheme> */}
         <BrowserRouter basename="/GooseTrack_frontend">
           <App />
         </BrowserRouter>
-      </ProviderTheme>
+      {/* </ProviderTheme> */}
       </PersistGate>
     </Provider>
   </React.StrictMode>
