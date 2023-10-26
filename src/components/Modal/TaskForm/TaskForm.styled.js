2d0c07d941b1a3cae38d6bf8fe07979ee61fd976
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-10-26 13:35:51 +0300

    Update TaskForm.styled.js

--- src/components/Modal/TaskForm/TaskForm.styled.js
+++ src/components/Modal/TaskForm/TaskForm.styled.js
@@ -229,7 +229,7 @@ export const RadioButtonInput = styled.input`
     position: absolute;
     height: 12px;
     width: 12px;
-    color: red;
+    color:  var(  --switch-active);
     outline: 2px solid;
 
     @media (min-width: 768px) {
